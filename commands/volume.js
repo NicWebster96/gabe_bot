10acef9abcddf82eabd78b941a369a16f4bce301
Nic Webster <NicWebster96@gmail.com>
2021-06-13 00:15:27 -0700

    npm packages install

--- commands/volume.js
+++ commands/volume.js
@@ -2,13 +2,14 @@ module.exports = {
   name: "volume",
   description: "Change volume of Gabe's bork!",
   execute(message, args) {
-    if (args.length != 2) {
+    if (args.length != 1) {
       return message.channel.send(
         'Set my volume by providing a number 1-10\nExample: "gabe volume 5"'
       );
     }
 
-    const requestedVolume = parseInt(args[1]);
+    console.log(args);
+    const requestedVolume = parseInt(args[0]);
     if (isNaN(requestedVolume)) {
       return message.reply("That doesn't seem to be a valid number");
     } else if (requestedVolume > 10 || requestedVolume < 1) {
@@ -17,6 +18,6 @@ module.exports = {
       );
     }
 
-    return message.channel.send(`Setting volume to ${requestedVolume}`);
+    return message.channel.send(`Setting volume to ${requestedVolume}. Bork!`);
   },
 };
