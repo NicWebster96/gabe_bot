ebd37b97a4b85439e62ca6c7d216780e48d547ad
Nic Webster <NicWebster96@gmail.com>
2021-06-12 22:56:47 -0700

    Moved event code to individual event files

--- index.js
+++ index.js
@@ -19,31 +19,21 @@ for (const file of commandFiles) {
   client.commands.set(command.name, command);
 }
 
+const eventFiles = fs
+  .readdirSync("./events")
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of eventFiles) {
+  const event = require(`./events/${file}`);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args, client));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
+  }
+}
+
 // retrieve bot token from .env file
 const TOKEN = process.env.TOKEN;
 
 // login to Discord with your app's token
 client.login(TOKEN);
-
-// when the client is ready, run this code
-// this event will only trigger one time after logging in
-client.once("ready", () => {
-  console.log("Ready!");
-});
-
-client.on("message", (message) => {
-  // if the message either doesn't start with the prefix or the author is a bot, exit early
-  if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-  const args = message.content.slice(prefix.length).trim().split(/ +/);
-  const command = args.shift().toLowerCase();
-
-  if (!client.commands.has(command)) return;
-
-  try {
-    client.commands.get(command).execute(message, args);
-  } catch (error) {
-    console.error(error);
-    message.reply("there was an error trying to execute that command!");
-  }
-});
