987fc6d56fad86a5518fcbf296b8413d3b93372d
Nic Webster <NicWebster96@gmail.com>
2021-06-12 23:04:22 -0700

    Accidentally removed message event content. Readded.

--- events/message.js
+++ events/message.js
@@ -1,8 +1,19 @@
 module.exports = {
   name: "message",
-  execute(message) {
-    console.log(
-      `${message.author.tag} in #${message.channel.name} sent: ${message.content}`
-    );
+  execute(message, client) {
+    // if the message either doesn't start with the prefix or the author is a bot, exit early
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if (!client.commands.has(command)) return;
+
+    try {
+      client.commands.get(command).execute(message, args);
+    } catch (error) {
+      console.error(error);
+      message.reply("there was an error trying to execute that command!");
+    }
   },
 };
