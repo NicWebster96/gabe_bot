5d6d8cb46cd5ad2a97fd864f458749c03434e1fd
Nic Webster <NicWebster96@gmail.com>
2021-06-12 22:43:46 -0700

    Added volume change command

--- index.js
+++ index.js
@@ -17,3 +17,32 @@ client.login(TOKEN);
 client.once("ready", () => {
   console.log("Ready!");
 });
+
+client.on("message", (message) => {
+  // if the message either doesn't start with the prefix or the author is a bot, exit early
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const command = args.shift().toLowerCase();
+
+  if (command === "volume") {
+    if (args.length != 2) {
+      return message.channel.send(
+        'Set my volume by providing a number 1-10\nExample: "gabe volume 5"'
+      );
+    }
+
+    const requestedVolume = parseInt(args[1]);
+    if (isNaN(requestedVolume)) {
+      return message.reply("That doesn't seem to be a valid number");
+    } else if (requestedVolume > 10 || requestedVolume < 1) {
+      return message.channel.send(
+        'Set my volume by providing a number 1-10\nExample: "gabe volume 5"'
+      );
+    }
+
+    return message.channel.send(`Setting volume to ${requestedVolume}`);
+  } else if (command === "info") {
+    return message.channel.send(`Set my volume with the \"volume\" command`);
+  }
+});
