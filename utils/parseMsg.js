2bfeae28d0600d1f0721dce0d6315cf2232b6917
Nic Webster <NicWebster96@gmail.com>
2021-06-29 23:53:14 -0700

    simply calling gabe with no commands was causing an error during parsing the entire message

--- utils/parseMsg.js
+++ utils/parseMsg.js
@@ -2,6 +2,10 @@ const { prefix } = require("../config.json");
 
 module.exports = (msg) => {
   const cmdTxt = msg.content.split(/\s/)[1];
+  if (!cmdTxt) {
+    // User simply typed "gabe" with no commands. Will be handled by message.js
+    return ["", ""];
+  }
   const suffix = msg.content.substring(prefix.length + cmdTxt.length + 2);
   // +2 for the space after white spaces
   return [cmdTxt, suffix];
